42d6f063ff53d7911074737d62c4c464b4a35085
Mark <mkriegsies1@live.maryville.edu>
2018-09-11 12:16:54 -0500

    question 1 finished

--- Lab - Extensions.playground/Pages/1. Exercise - Extensions.xcplaygroundpage/Contents.swift
+++ Lab - Extensions.playground/Pages/1. Exercise - Extensions.xcplaygroundpage/Contents.swift
@@ -3,13 +3,35 @@
  
  Define an extension to `Character` that includes a function `isVowel()`. The function returns `true` if the character is a vowel (a,e,i,o,u), and `false` otherwise. Be sure to properly handle uppercase and lowercase characters.
  */
+var testCharacter: Character = "E"
+
+        //ensures the character is lowercased
+func lowercaseTheLetter() -> Character {
+    let testCharacterLowered: String = String(testCharacter)
+        testCharacter = Character(testCharacterLowered.lowercased())
+    return testCharacter
+    
+}
+    lowercaseTheLetter()
+
+
+extension Character {
+    mutating func isVowel() -> Bool {
+                //tests to see if its a vowel (contains a e i o or u)
+        if testCharacter == "a" ||  testCharacter == "e" || testCharacter == "i" || testCharacter == "o" || testCharacter == "u"
+            {
+               return true
+        }
+        return false
+    }
+}
+testCharacter.isVowel()
 
 
 /*:
  Create two `Character` constants, `myVowel` and `myConsonant`, and set them to a vowel and a consonant, respectively. Use the `isVowel()` methods on each constant to determine whether or not it's a vowel.
  */
-
-
+//print("test to see if xCode works")
 /*:
  Create a `Rectangle` struct with two variable properties, `length` and `width`, both of type `Double`. Below the definition, write an extension to `Rectangle` that includes a function, `halved()`. This function returns a new `Rectangle` instance with half the length and half the width of the original rectangle.
  */
